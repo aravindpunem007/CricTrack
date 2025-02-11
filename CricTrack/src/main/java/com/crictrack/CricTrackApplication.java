aca0ccc0a75be2dfaf32c1c74c865f1e911202e7
Aravind Punem <apunem@uab.edu>
2025-02-10 21:14:48 -0600

    "Initial commit - Setup project structure"

---
+++ CricTrack/src/main/java/com/crictrack/CricTrackApplication.java
@@ -0,0 +1,13 @@
+package com.crictrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricTrackApplication.class, args);
+	}
+
+}
---
+++ CricTrack/src/test/java/com/crictrack/CricTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crictrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
